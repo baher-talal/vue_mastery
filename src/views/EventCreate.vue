51ec16f22617f9897abc0f20887361049086236d
Baher Talal <57946827+baher-talal@users.noreply.github.com>
2021-04-01 16:48:56 +0200

    last

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Create an Evebt, {{ user.name }}</h1>
+    <p>this event was created by {{ user.id }}</p>
+    <p>there are {{ getEventById(1) }}</p>
+
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters (['getEventById']),
+      ...mapState (['user', 'categories'])
+  }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
